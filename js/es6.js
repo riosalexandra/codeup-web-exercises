f7e258c2dc6b136b73bbc330b7cf96d9a69af1b2
AlexRios <riosalexandra91@gmail.com>
2022-05-25 21:28:32 -0500

    corrected errors

--- js/es6.js
+++ js/es6.js
@@ -78,15 +78,15 @@ users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+  // const name = user.name;
+  // const email = user.email;
+  // const languages = user.languages;
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
-});
+//   developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+// });
 
-developers.push(`${name}`s email is ${email); ${languages.join(', ')}`);
+  developers.push(`${name}'s email is ${email}; ${name} knows ${languages.join(', ')}`);
 });
 
 
